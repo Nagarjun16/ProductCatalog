a87eca33342e44ec7609b93f11100d66f0ba5d11
Nagarjun <nagarjun16@gmail.com>
2020-05-23 23:33:58 +0530

    expression has changed after it was checked. previous value -Fix

--- UIPortal/src/app/components/commons-module/loader/loader.component.ts
+++ UIPortal/src/app/components/commons-module/loader/loader.component.ts
@@ -1,21 +1,39 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
 import { Subject } from 'rxjs/internal/Subject';
 import { LoaderService } from 'src/app/services/Loader.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-loader',
   templateUrl: './loader.component.html',
   styleUrls: ['./loader.component.css']
 })
-export class LoaderComponent implements OnInit {
+export class LoaderComponent implements OnInit, AfterViewInit {
   @Input() color = 'primary';
   @Input() mode = 'indeterminate';
   @Input() value = 50;
-  isLoading: Subject<boolean> = this.loaderService.isLoading;
+
+  // tslint:disable-next-line: variable-name
+  _isLoading = false;
+  loadingSubscription: Subscription;
 
   constructor(private loaderService: LoaderService) { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit(){
+    this.loadingSubscription = this.loaderService.isLoading.subscribe((value=>{
+      this.isLoading = value;
+    }));
+  }
+
+  get isLoading(): boolean{
+    return this._isLoading;
+  }
+
+  set isLoading(value) {
+    this._isLoading = value;
   }
 
 }
